e25f1a53a2edf6eb2ea9fbffc7a3821df70c362e
Revanth686 <revanth3503@gmail.com>
2024-05-21 01:56:45 +0530

    init

---
+++ index.js
@@ -0,0 +1,186 @@
+import express from "express";
+import path from "path";
+import archiver from "archiver";
+import fs from "fs";
+import QRCode from "qrcode";
+import multer from "multer";
+import chalk from "chalk";
+
+const downloadServer = express(),
+  uploadServer = express();
+let filePath, storagePath, message;
+const storage = multer.diskStorage({
+  destination: (_, file, cb) => {
+    cb(null, storagePath);
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+uploadServer.use(express.static(new URL("./public", import.meta.url).pathname));
+uploadServer.set("views", new URL("./views", import.meta.url).pathname);
+uploadServer.set("view engine", "ejs");
+const upload = multer({ storage: storage }).single("file");
+
+const startLogs = (mode) => {
+  const date = new Date();
+  const formatData = (input) => {
+    if (input > 9) return input;
+    else return `0${input}`;
+  };
+  const formatHour = (input) => {
+    if (input > 12) return input - 12;
+    return input;
+  };
+  (!mode ? downloadServer : uploadServer).use((req, _, next) => {
+    const dd = formatData(date.getDate()),
+      mm = formatData(date.getMonth() + 1),
+      yyyy = date.getFullYear(),
+      hh = formatData(formatHour(date.getHours())),
+      MM = formatData(date.getMinutes()),
+      SS = formatData(date.getSeconds());
+    console.log(
+      `${req.ip} - [${dd}/${mm}/${yyyy}:${hh}:${MM}:${SS}] "${req.method} ${req.path} ${req.protocol}"`,
+    );
+    next();
+  });
+};
+
+downloadServer.get("/message", (req, res) => {
+  try {
+    res.status(200).send(message);
+    console.log(`message sent successfully to ${chalk.bold(req.ip)}`);
+  } catch (err) {
+    console.error(`error sending message \n${chalk.red(err)}`);
+  }
+});
+
+downloadServer.get("/file/:filename", (req, res) => {
+  const { filename } = req.params;
+  try {
+    res.setHeader("Content-Disposition", `attachment; filename=${filename}`);
+    const stats = fs.statSync(filePath);
+    if (stats.isFile()) {
+      const r = fs.createReadStream(filePath);
+      r.pipe(res);
+      r.on("end", () => {
+        console.log(
+          `successfully transfered ${chalk.bold(filename)} to ${chalk.bold(req.ip)}`,
+        );
+      });
+    } else if (stats.isDirectory()) {
+      res.setHeader(
+        "Content-Type",
+        "application/zip;",
+        `filename=${filename}.zip`,
+      );
+      const archive = archiver("zip", {
+        zlib: { level: 9 },
+      });
+      archive.on("error", (err) => {
+        console.log(`error in archiving ${err}`);
+      });
+      archive.pipe(res);
+      archive.directory(filePath, false);
+      archive.on("finish", () => {
+        console.log(
+          `successfully transfered ${chalk.bold(filename)} to ${chalk.bold(req.ip)}`,
+        );
+      });
+      archive.finalize();
+    }
+  } catch (err) {
+    console.error(
+      `error in uploading requested file/folder\n ${chalk.red(err)}`,
+    );
+  }
+});
+uploadServer.get("/", (_, res) => {
+  res.render("upload");
+});
+
+uploadServer.post(
+  "/",
+  (req, res, next) => {
+    upload(req, res, (err) => {
+      if (err instanceof multer.MulterError) {
+        throw err;
+      } else if (err) {
+        console.log(`Unknown error occured when uploading \n${chalk.red(err)}`);
+        res.status(500).json({
+          Error: `Unknown error occured when uploading \n${chalk.red(err)}`,
+        });
+        process.exit(1);
+      }
+      next();
+    });
+  },
+  (req, res) => {
+    res.redirect("/");
+    console.log(
+      `File ${chalk.bold(req.file.originalname)} uploaded successfully to ${chalk.bold(path.basename(req.file.destination))}`,
+    );
+  },
+);
+
+export const startDownloadServer = async ({ ...args }) => {
+  const absPath = path.normalize(path.resolve(args.filePath));
+  const filename = path.basename(absPath);
+  const address = args.address;
+  const port = args.port;
+  const url = `http://${address}:${port}/file/${filename}`;
+  filePath = absPath;
+  downloadServer.listen(port, address, () => {
+    console.log(`Scan the following QR to start downloading`);
+    QRCode.toString(
+      url,
+      { type: "terminal", small: true },
+      function (err, url) {
+        if (err) console.error(err);
+        else console.log(url);
+      },
+    );
+
+    args.debug && console.log(url);
+    startLogs(args.mode);
+  });
+};
+export const startUploadServer = async ({ ...args }) => {
+  const absPath = path.normalize(path.resolve(args.filePath));
+  const address = args.address;
+  const port = args.port;
+  const url = `http://${address}:${port}/`;
+  storagePath = absPath;
+  uploadServer.listen(port, address, () => {
+    console.log(`Scan the following QR to start uploading`);
+    QRCode.toString(
+      url,
+      { type: "terminal", small: true },
+      function (err, url) {
+        if (err) console.error(err);
+        else console.log(url);
+      },
+    );
+    args.debug && console.log(url);
+    startLogs(args.mode);
+  });
+};
+export const sendMessage = ({ ...args }) => {
+  const port = args.port;
+  const address = args.address;
+  const url = `http://${address}:${port}/message`;
+  message = args.msg;
+  downloadServer.listen(port, address, () => {
+    console.log(`Scan the following QR to receive message `);
+    QRCode.toString(
+      url,
+      { type: "terminal", small: true },
+      function (err, url) {
+        if (err) console.error(err);
+        else console.log(url);
+      },
+    );
+    console.log(url);
+    startLogs(args.mode);
+  });
+};
---
+++ ip.js
@@ -0,0 +1,251 @@
+#! /usr/bin/env node
+import os from "os";
+import network from "network";
+import child_process from "child_process";
+import http from "http";
+import fs from "fs";
+import path from "path";
+import chalk from "chalk";
+import meow from "meow";
+import {
+  startDownloadServer,
+  startUploadServer,
+  sendMessage,
+} from "./index.js";
+
+const getLocalIp = () => {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === "IPv4" && !iface.internal) {
+        return iface.address;
+      }
+    }
+  }
+  console.log("network is unreachable");
+  process.exit(1);
+};
+
+function getLocalIpsAvailable(cb) {
+  network.get_interfaces_list((err, interfaces) => {
+    if (err) {
+      cb(err, null);
+      return;
+    }
+    const data = interfaces
+      .filter((iface) => !iface.internal)
+      .map((iface) => iface.ip_address)
+      .filter((ip) => ip);
+    cb(null, data);
+  });
+}
+
+const getSSID = () => {
+  const WINDOWS = "win32",
+    MACOS = "darwin",
+    LINUX = "linux";
+  const os = process.platform;
+  try {
+    if (os === LINUX) {
+      const ssid = child_process
+        .execSync("iwgetid -r 2>/dev/null")
+        .toString()
+        .trim();
+      return ssid;
+    } else if (os === MACOS) {
+      ssid = child_process
+        .execSync(
+          "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {print substr($0, index($0, $2))}'",
+        )
+        .toString()
+        .trim();
+      return ssid;
+    } else if (os === WINDOWS) {
+      //note that if WiFi is not connected, Profile line will not be found and nothing will be returned.
+      let interface_info = child_process
+        .execSync("netsh.exe wlan show interfaces")
+        .toString();
+      for (let line of interface_info.split("\n")) {
+        if (line.trim().startsWith("Profile")) {
+          ssid = line.split(":")[1].trim();
+          return ssid;
+        }
+      }
+    }
+    return null;
+  } catch (err) {
+    console.log(`err finding ssid: ${err} ${os}`);
+  }
+};
+
+//choose a port from this range to avoid conflicts with well-known ports.
+const getRandomPort = (max = 65535, min = 1024) => {
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
+const findAvaliablePort = () => {
+  return new Promise((resolve, reject) => {
+    const port = getRandomPort();
+    const server = http.createServer();
+    server.listen(port, () => {
+      server.on("close", () => {
+        resolve(port);
+      });
+      server.close();
+    });
+    server.on("error", (err) => {
+      if (err.code === "EADDRINUSE") {
+        console.log(`${port} already in use, searching for another one\n`);
+        findAvaliablePort().then(resolve).catch(reject);
+      } else {
+        reject("error finding port");
+      }
+    });
+  });
+};
+
+const main = async () => {
+  let port, address, filePath, mode, debug, cli, message;
+  const helpText = `
+		qrxfr - cli tool to share files btw devices in same network by scanning a qr
+		${chalk.bold("Usage:")} 
+    ${chalk.italic("- message passing:")} qrxfr [--message MESSAGE]
+      $ qrxfr --message,-m MESSAGE
+
+    ${chalk.italic("- file sharing: ")} qrxfr [option {value}] <path>
+      $ qrxfr /path/to/file || /path/to/folder
+      $ qrxfr --recieve,-r /path/to/receive/file/to
+      $ qrxfr --port,-p PORT
+      $ qrxfr --ip,-i IP_ADDRESS
+      $ qrxfr --help
+      $ qrxfr --version
+
+		${chalk.bold("Options:")}
+      -r --receive \tenable upload mode, given path's used to store recieved file, defaults to pwd
+      -p --port    \tuse a custom port for server; note: 1024<=PORT<=65535 to avoid conflicting errors
+      -i --ip      \tbind webserver to custom ip, must be accessible from within subnetwork 
+      -m --message \tsend message to client
+      -h --help    \tshow this screen
+      -v --version \tshow version
+
+		${chalk.bold("Examples:")}
+      $ qrxfr ./assignment.pdf
+      $ qrxfr one.txt -p 4000 --ip 192.168.1.10
+      $ qrxfr sampleFolder
+      $ qrxfr --receive ./Downloads
+      $ qrxfr -r
+      $ qrxfr -r ./Downloads -p 5000 -i 192.168.200.23
+      $ qrxfr -m "Kaizoku ou ni ore wa naru"
+      $ qrxfr --message "messaging..." -p 4000 -i 192.168.12.24
+	`;
+  getLocalIpsAvailable(async (err, ips) => {
+    if (err) {
+    } else {
+      cli = meow({
+        importMeta: import.meta,
+        help: helpText,
+        flags: {
+          ip: {
+            type: "string",
+            default: getLocalIp(),
+            choices: ips,
+            shortFlag: "i",
+            aliases: ["IP", "iP"],
+            isMultiple: false,
+          },
+          port: {
+            type: "number",
+            default: 1023,
+            shortFlag: "p",
+            aliases: ["Port", "PORT"],
+            isMultiple: false,
+          },
+          message: {
+            type: "boolean",
+            default: false,
+            shortFlag: "m",
+            aliases: ["msg"],
+            isMultiple: false,
+          },
+          help: {
+            type: "boolean",
+            default: false,
+            shortFlag: "h",
+          },
+          version: {
+            type: "boolean",
+            default: false,
+            shortFlag: "v",
+          },
+          receive: {
+            type: "boolean",
+            default: false,
+            shortFlag: "r",
+          },
+        },
+        description: false,
+      });
+      debug = true;
+      mode = cli.flags.r || cli.flags.receive;
+      port = cli.flags.port || cli.flags.p || cli.flags.Port || cli.flags.PORT;
+      address = cli.flags.ip || cli.flags.i || cli.flags.IP || cli.flags.iP;
+      message = cli.flags.message || cli.flags.m || cli.flags.msg;
+      filePath = cli.input[0];
+      const msg = cli.input?.join(" ");
+      if (!message) {
+        if (!filePath && mode) {
+          filePath = ".";
+        }
+        if (!fs.existsSync(filePath)) {
+          console.log(chalk.red.bold(`No such file or directory ${filePath}`));
+          if (!mode)
+            console.log(chalk.red.bold(`specify a valid file to send`));
+          if (mode)
+            console.log(chalk.red.bold(`specify a folder to receive to`));
+          console.log(chalk.red(`see qrxfr --help for more info`));
+          process.exit(1);
+        }
+        const stats = fs.statSync(path.normalize(path.resolve(filePath)));
+        if (mode && !stats.isDirectory()) {
+          console.log(chalk.red.bold(`${filePath} is not a folder`));
+          console.log(chalk.red(`see qrxfr --help for more info`));
+          process.exit(1);
+        }
+        if (
+          mode &&
+          path.basename(path.normalize(path.resolve(filePath))) == "public"
+        ) {
+          console.log(chalk.red.bold(`cannot choose public folder to receive`));
+          process.exit(1);
+        }
+      }
+      if (message && !msg) {
+        console.log(chalk.red.bold(`enter valid message to send`));
+        console.log(chalk.red(`see qrxfr --help for more info`));
+        process.exit(1);
+      }
+      if (!port || port < 1024) {
+        try {
+          port = await findAvaliablePort();
+        } catch (err) {
+          console.log(`failed to find available port, try again later ${err}`);
+          process.exit(1);
+        }
+      }
+      if (!address) {
+        address = getLocalIp();
+      }
+      const ssid = getSSID();
+      console.log(
+        `make sure your device is connected to ${chalk.blue.bold(ssid)}`,
+      );
+      if (message) {
+        sendMessage({ port, address, debug, msg });
+      } else if (mode) {
+        startUploadServer({ filePath, port, address, debug, mode: mode });
+      } else
+        startDownloadServer({ filePath, port, address, debug, mode: null });
+    }
+  });
+};
+main();
